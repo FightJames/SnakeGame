c1e636ee299ada209bfe133ca9a934d60762d3e2
James <as1234ert@gmail.com>
2019-09-19 16:52:29 +0800

    Add hit body judgement

--- app/src/main/java/com/jamestech/snakegame/model/Snake.kt
+++ app/src/main/java/com/jamestech/snakegame/model/Snake.kt
@@ -1,53 +1,101 @@
 package com.jamestech.snakegame.model
 
 import android.util.Log
+import com.jamestech.snakegame.view.GameView
 import java.util.*
 
 
-// snake is composed by pointBean
+// snake is composed by point
 class Snake {
     var points: Queue<Point> = LinkedList<Point>()
     val initX = 0
     val initY = 0
     var endX: Int = Integer.MAX_VALUE
     var endY: Int = Integer.MAX_VALUE
-    private var move = 5
+    var snakeWay: GameView.SnakeWay = GameView.SnakeWay.LEFT
+    val snakeBodyWidth = 30
 
-    fun moveLeft() {
-        val priPoint = points.poll()
+    fun moveLeft(): Boolean {
+        snakeWay = GameView.SnakeWay.LEFT
+        val newPoint = Point(points.last().x - snakeBodyWidth, points.last().y)
+        points.poll()
         Log.d("Left size ", points.size.toString())
-        if (priPoint.x - move < initX) {
-            points.add(Point(endX, priPoint.y))
-            return
+        if (newPoint.x < initX) {
+            points.add(Point(endX, newPoint.y))
+        } else {
+            points.add(newPoint)
         }
-        points.add(Point(priPoint.x - move, priPoint.y))
+        return hitBody(points.last())
     }
 
-    fun moveRight() {
-        Log.d("Right size ", points.size.toString())
-        val priPoint = points.poll()
-        if (priPoint.x + move > endX) {
-            points.add(Point(initX, priPoint.y))
-            return
+    fun moveRight(): Boolean {
+        snakeWay = GameView.SnakeWay.RIGHT
+        val newPoint = Point(points.last().x + snakeBodyWidth, points.last().y)
+        points.poll()
+        if (newPoint.x > endX) {
+            points.add(Point(initX, newPoint.y))
+        } else {
+            points.add(newPoint)
         }
-        points.add(Point(priPoint.x + move, priPoint.y))
+        return hitBody(points.last())
     }
 
-    fun moveUp() {
-        val priPoint = points.poll()
-        if (priPoint.y - move < initY) {
-            points.add(Point(priPoint.x, endY))
-            return
+    fun moveUp(): Boolean {
+        snakeWay = GameView.SnakeWay.UP
+        val newPoint = Point(points.last().x, points.last().y - snakeBodyWidth)
+        points.poll()
+        if (newPoint.y < initY) {
+            points.add(Point(newPoint.x, endY - snakeBodyWidth))
+        } else {
+            points.add(newPoint)
         }
-        points.add(Point(priPoint.x, priPoint.y - move))
+        return hitBody(points.last())
     }
 
-    fun moveDown() {
-        val priPoint = points.poll()
-        if (priPoint.y + move > endY) {
-            points.add(Point(priPoint.x, initY))
-            return
+    fun moveDown(): Boolean {
+        snakeWay = GameView.SnakeWay.DOWN
+        val newPoint = Point(points.last().x, points.last().y + snakeBodyWidth)
+        points.poll()
+        if (newPoint.y > endY) {
+            points.add(Point(newPoint.x, initY))
+        } else {
+            points.add(newPoint)
         }
-        points.add(Point(priPoint.x, priPoint.y + move))
+        return hitBody(points.last())
+    }
+
+    fun growUpOneBody() {
+        val point = points.last()
+        val newPoint: Point
+        when (snakeWay) {
+            GameView.SnakeWay.DOWN -> {
+                newPoint = Point(point.x, point.y + snakeBodyWidth)
+            }
+            GameView.SnakeWay.UP -> {
+                newPoint = Point(point.x, point.y - snakeBodyWidth)
+            }
+            GameView.SnakeWay.RIGHT -> {
+                newPoint = Point(point.x + snakeBodyWidth, point.y)
+            }
+            GameView.SnakeWay.LEFT -> {
+                newPoint = Point(point.x - snakeBodyWidth, point.y)
+            }
+        }
+        points.add(newPoint)
+    }
+
+    fun hitBody(point: Point): Boolean {
+        points.forEachIndexed { index, element ->
+            if (index + 3 < points.size) {
+                if (element.x <= point.x && point.x < element.x + snakeBodyWidth
+                    && element.y <= point.y && point.y < element.y + snakeBodyWidth
+                ) {
+                    return true
+                }
+            } else {
+                return@forEachIndexed
+            }
+        }
+        return false
     }
 }
--- app/src/main/java/com/jamestech/snakegame/view/GameView.kt
+++ app/src/main/java/com/jamestech/snakegame/view/GameView.kt
@@ -9,94 +9,44 @@ import android.util.Log
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
+import android.widget.Toast
 import com.jamestech.snakegame.model.Point
 import com.jamestech.snakegame.model.Snake
+import java.util.*
 
 class GameView : View, GestureDetector.OnGestureListener {
-    override fun onShowPress(e: MotionEvent?) {
-        Log.d("James snake", " onShowPress")
-    }
-
-    override fun onSingleTapUp(e: MotionEvent?): Boolean {
-        Log.d("James snake", " onSingleTapUp")
-        return false
-    }
-
-
-    override fun onDown(e: MotionEvent?): Boolean {
-        Log.d("James snake", " onDown")
-        return true
-    }
-
-
-    override fun onFling(
-        e1: MotionEvent?,
-        e2: MotionEvent?,
-        velocityX: Float,
-        velocityY: Float
-    ): Boolean {
-
-        Log.d("James snake", " onFling")
-        if (e1 == null || e2 == null) {
-            return false
-        }
-        val diffX = e2.x - e1.x
-        val diffY = e2.y - e1.y
-        if (Math.abs(diffX) > Math.abs(diffY)) {
-            when {
-                diffX > 0 -> {
-                    if (snakeWay != SnakeWay.LEFT)
-                        snakeWay = SnakeWay.RIGHT
-                    Log.d("James way", " Right")
-                }
-                else -> {
-                    if (snakeWay != SnakeWay.RIGHT)
-                        snakeWay = SnakeWay.LEFT
-                    Log.d("James way", " Left")
-                }
-            }
-            return true
-        }
-        when {
-            diffY > 0 -> {
-                if (snakeWay != SnakeWay.UP)
-                    snakeWay = SnakeWay.DOWN
-                Log.d("James way", " Down")
-            }
-            else -> {
-                if (snakeWay != SnakeWay.DOWN)
-                    snakeWay = SnakeWay.UP
-                Log.d("James way", " Up")
-            }
-        }
-        return true
-    }
-
-    override fun onScroll(
-        e1: MotionEvent?,
-        e2: MotionEvent?,
-        distanceX: Float,
-        distanceY: Float
-    ): Boolean {
-        Log.d("James snake", " onShowPress")
-        return false
-    }
-
-    override fun onLongPress(e: MotionEvent?) {
-        Log.d("James snake", " onLongPress")
-    }
-
-    private var snakeWay: SnakeWay = SnakeWay.RIGHT
     private var isFailed = false
     private var paint: Paint = Paint()
+    private var priSnakeWay: SnakeWay = SnakeWay.LEFT
+    private val taskQueue: Queue<SnakeWay> by lazy {
+        var list = LinkedList<SnakeWay>()
+        list.add(SnakeWay.LEFT)
+        list
+    }
     private val gestureDetector: GestureDetector by lazy {
         GestureDetector(this.context, this)
     }
-    private val snake: Snake by lazy {
+
+    val snake: Snake by lazy {
         Snake().apply {
+            snakeWay = priSnakeWay
             points.add(
                 Point(
                     this@GameView.measuredWidth / 2,
+                    this@GameView.measuredHeight / 2
+                )
+            )
+
+            points.add(
+                Point(
+                    this@GameView.measuredWidth / 2 + snakeBodyWidth,
+                    this@GameView.measuredHeight / 2
+                )
+            )
+
+            points.add(
+                Point(
+                    this@GameView.measuredWidth / 2 + snakeBodyWidth * 2,
                     this@GameView.measuredHeight / 2
                 )
             )
@@ -129,6 +79,11 @@ class GameView : View, GestureDetector.OnGestureListener {
     }
 
     private fun init() {
+        this.setOnClickListener {
+            snake.growUpOneBody()
+            Log.d("Hello", " click")
+            false
+        }
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -138,37 +93,141 @@ class GameView : View, GestureDetector.OnGestureListener {
     }
 
     override fun onDraw(canvas: Canvas) {
+        postDelayed({
+            invalidate()
+        }, 500)
         paint.color = Color.RED
         drawSnake(canvas)
         paint.color = Color.GREEN
-
-        invalidate()
     }
 
     private fun drawSnake(canvas: Canvas) {
-        when (snakeWay) {
-            SnakeWay.UP -> snake.moveUp()
-            SnakeWay.DOWN -> snake.moveDown()
-            SnakeWay.LEFT -> snake.moveLeft()
-            SnakeWay.RIGHT -> snake.moveRight()
+        if (taskQueue.isEmpty().not()) {
+            priSnakeWay = taskQueue.poll()
+        }
+        var execute = true
+        when (priSnakeWay) {
+            SnakeWay.UP -> {
+                execute = snake.moveUp()
+            }
+            SnakeWay.DOWN -> {
+                execute = snake.moveDown()
+            }
+            SnakeWay.LEFT -> {
+                execute = snake.moveLeft()
+            }
+            SnakeWay.RIGHT -> {
+                execute = snake.moveRight()
+            }
         }
         snake.points.forEach {
             canvas.drawRect(
                 it.x.toFloat(),
                 it.y.toFloat(),
-                (it.x + 30).toFloat(),
-                (it.y + 30).toFloat(),
+                (it.x + snake.snakeBodyWidth).toFloat(),
+                (it.y + snake.snakeBodyWidth).toFloat(),
                 paint
             )
+        }
+        if (execute) {
+            Toast.makeText(this.context, "Game over", Toast.LENGTH_LONG).show()
+            this.handler.removeCallbacksAndMessages(null)
         }
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         Log.d("James snake", " touch event")
-        return gestureDetector.onTouchEvent(event)
+        gestureDetector.onTouchEvent(event)
+        return true
     }
 
     enum class SnakeWay {
         LEFT, RIGHT, UP, DOWN
     }
+
+    override fun onShowPress(e: MotionEvent?) {
+        Log.d("James snake", " onShowPress")
+    }
+
+    override fun onSingleTapUp(e: MotionEvent?): Boolean {
+        Log.d("James snake", " onSingleTapUp")
+        return false
+    }
+
+
+    override fun onDown(e: MotionEvent?): Boolean {
+        Log.d("James snake", " onDown")
+        return true
+    }
+
+
+    override fun onFling(
+        e1: MotionEvent?,
+        e2: MotionEvent?,
+        velocityX: Float,
+        velocityY: Float
+    ): Boolean {
+
+        Log.d("James snake", " onFling")
+        if (e1 == null || e2 == null) {
+            return false
+        }
+        val diffX = e2.x - e1.x
+        val diffY = e2.y - e1.y
+        if (Math.abs(diffX) > Math.abs(diffY)) {
+            when {
+                diffX > 0 -> {
+                    if (taskQueue.isEmpty().not() && taskQueue.last() != SnakeWay.LEFT) {
+                        taskQueue.add(SnakeWay.RIGHT)
+                    } else if (priSnakeWay != SnakeWay.LEFT) {
+                        taskQueue.add(SnakeWay.RIGHT)
+                    }
+                    Log.d("James way", " Right")
+                }
+                else -> {
+                    if (taskQueue.isEmpty().not() && taskQueue.last() != SnakeWay.RIGHT) {
+                        taskQueue.add(SnakeWay.LEFT)
+                    } else if (priSnakeWay != SnakeWay.RIGHT) {
+                        taskQueue.add(SnakeWay.LEFT)
+                    }
+                    Log.d("James way", " Left")
+                }
+            }
+            return true
+        }
+        when {
+            diffY > 0 -> {
+                if (taskQueue.isEmpty().not() && taskQueue.last() != SnakeWay.UP) {
+                    taskQueue.add(SnakeWay.DOWN)
+                } else if (priSnakeWay != SnakeWay.UP) {
+                    taskQueue.add(SnakeWay.DOWN)
+                }
+                Log.d("James way", " Down")
+            }
+            else -> {
+                if (taskQueue.isEmpty().not() && taskQueue.last() != SnakeWay.DOWN) {
+                    taskQueue.add(SnakeWay.UP)
+                } else if (priSnakeWay != SnakeWay.DOWN) {
+                    taskQueue.add(SnakeWay.UP)
+                }
+                Log.d("James way", " Up")
+            }
+        }
+        return true
+    }
+
+    override fun onScroll(
+        e1: MotionEvent?,
+        e2: MotionEvent?,
+        distanceX: Float,
+        distanceY: Float
+    ): Boolean {
+        Log.d("James snake", " onShowPress")
+        return false
+    }
+
+    override fun onLongPress(e: MotionEvent?) {
+        Log.d("James snake", " onLongPress")
+    }
+
 }
